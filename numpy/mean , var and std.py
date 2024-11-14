04ba75a4727a3fce835f25c95d49acdb8cfd5dfe
ZAM33L <125803542+ZAM33L@users.noreply.github.com>
2024-11-14 12:35:03 +0530

    Add files via upload

---
+++ numpy/mean , var and std.py
@@ -0,0 +1,11 @@
+import numpy
+
+n, m = map(int, input().split())
+ar = []
+for _ in range(n):
+    tmp = list(map(int, input().split()))
+    ar.append(tmp)
+np_ar = numpy.array(ar)
+print(numpy.mean(np_ar, axis=1))
+print(numpy.var(np_ar, axis=0))
+print(round(numpy.std(np_ar, axis=None), 11))
